74b023a00780f07100083461603a8daf6ad72213
l.j <duanlianjie@icourt.cc>
2019-08-13 11:32:29 +0800

    fix data struct

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -5,110 +5,153 @@ Page({
    * 页面的初始数据
    */
   data: {
-    comments:[
-      {commentId:1,
-      comment:"天气真是非常好, 一起出去郊游",
-      userId:"xxxxx",
-      userName:"张三",
-      commentTime:"2019-09-12 10:32",
-      type : 1
-      },
-      {
-        commentId: 2,
-        comment: "那天我看见一场飓风，它像棉被一样被一只巨手牢牢地捂住天空的呼吸，直到疲惫得动弹不得。天色暗得仿佛会一直熄灭下去。我在等待着一场迟迟不来的雨，将这个世界的喧嚣与浮躁冲刷殆尽。",
-        userId: "xxxxx",
-        userName: "张三",
-        commentTime: "2019-09-12 10:32",
-        type: 0
-      },
-      {
-        commentId: 3,
-        comment: "香港的饭馆里大红大绿大金大银，语声喧哗，北人皆以为俗气，其实你读唐诗，正是这种世俗的热闹，铺张而有元气。",
-        userId: "xxxxx",
-        userName: "张三",
-        commentTime: "2019-09-12 10:32",
-        type: 0
-      },
-      {
-        commentId: 4,
-        comment: "天气真是非常好, 一起出去郊游",
-        userId: "xxxxx",
-        userName: "张三",
-        commentTime: "2019-09-12 10:32",
-        type: 1
-      },
-      {
-        commentId: 5,
-        comment: "我喜欢孔子的入世，入得很清晰，有智慧，含幽默，实实在在不标榜。道家则总有点标榜的味道，从古到今，不断地有人用道家来标榜自己，因为实在是太方便了。",
-        userId: "xxxxx",
-        userName: "张三",
-        commentTime: "2019-09-12 10:32",
-        type: 2
-      },
-      {
-        commentId: 6,
-        comment: "天气真是非常好, 一起出去郊游",
-        userId: "xxxxx",
-        userName: "张三",
-        commentTime: "2019-09-12 10:32",
-        type: 1
-      },
-    ],
+    comments: [{
+      action: null,
+      approveId: "13708",
+      approveType: 1,
+      content: "qqqq",
+      createUserId: "A325ACCB789711E998137CD30AEB153E",
+      createUserName: "吴彦祖",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/d9b9e54d-262b-4a2a-b28a-448a3d13452c",
+      feedType: "1",
+      gmtCreate: 1563539393000,
+      gmtModified: 1563539393000,
+      id: 85568,
+      modifyUserId: "A325ACCB789711E998137CD30AEB153E"
+    }, {
+      action: null,
+      approveId: "13708",
+      approveType: 0,
+      content: "ffffff",
+      createUserId: "C3095D1F789711E998137CD30AEB153E",
+      createUserName: "张帆",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/5eff20f7-8bab-4a05-a8a8-f7b47b7474a7",
+      feedType: "1",
+      gmtCreate: 1563538987000,
+      gmtModified: 1563538987000,
+      id: 85566,
+      modifyUserId: "C3095D1F789711E998137CD30AEB153E"
+    }, {
+      action: null,
+      approveId: "13708",
+      approveType: 0,
+        content: "我喜欢孔子的入世，入得很清晰，有智慧，含幽默，实实在在不标榜。道家则总有点标榜的味道，从古到今，不断地有人用道家来标榜自己，因为实在是太方便了。",
+      createUserId: "C3095D1F789711E998137CD30AEB153E",
+      createUserName: "张帆",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/5eff20f7-8bab-4a05-a8a8-f7b47b7474a7",
+      feedType: "1",
+      gmtCreate: 1563538896000,
+      gmtModified: 1563538896000,
+      id: 85562,
+      modifyUserId: "C3095D1F789711E998137CD30AEB153E"
+    }, {
+      action: null,
+      approveId: "13708",
+      approveType: 2,
+      content: "你们的",
+      createUserId: "A325ACCB789711E998137CD30AEB153E",
+      createUserName: "吴彦祖",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/d9b9e54d-262b-4a2a-b28a-448a3d13452c",
+      feedType: "1",
+      gmtCreate: 1563538882000,
+      gmtModified: 1563538882000,
+      id: 85561,
+      modifyUserId: "A325ACCB789711E998137CD30AEB153E"
+    }, {
+      action: null,
+      approveId: "13708",
+      approveType: 0,
+        content: "我喜欢孔子的入世，入得很清晰，有智慧，含幽默，实实在在不标榜。道家则总有点标榜的味道，从古到今，不断地有人用道家来标榜自己，因为实在是太方便了。",
+      createUserId: "A325ACCB789711E998137CD30AEB153E",
+      createUserName: "吴彦祖",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/d9b9e54d-262b-4a2a-b28a-448a3d13452c",
+      feedType: "1",
+      gmtCreate: 1563538872000,
+      gmtModified: 1563538872000,
+      id: 85560,
+      modifyUserId: "A325ACCB789711E998137CD30AEB153E"
+    }, {
+      action: null,
+      approveId: "13708",
+      approveType: 2,
+      content: "你们，\n",
+      createUserId: "A325ACCB789711E998137CD30AEB153E",
+      createUserName: "吴彦祖",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/d9b9e54d-262b-4a2a-b28a-448a3d13452c",
+      feedType: "1",
+      gmtCreate: 1563538866000,
+      gmtModified: 1563538866000,
+      id: 85559,
+      modifyUserId: "A325ACCB789711E998137CD30AEB153E"
+    }, {
+      action: "CREATE",
+      approveId: "13708",
+      approveType: 2,
+      content: "吴彦祖 发起了审批",
+      createUserId: "A325ACCB789711E998137CD30AEB153E",
+      createUserName: "吴彦祖",
+      createUserPic: "https://testres.alphalawyer.cn/paralegalInfo/d9b9e54d-262b-4a2a-b28a-448a3d13452c",
+      feedType: "2",
+      gmtCreate: 1563538816000,
+      gmtModified: 1563538816000,
+      id: 85557,
+      modifyUserId: "A325ACCB789711E998137CD30AEB153E"
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
