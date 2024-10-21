f734b3faec639a94e4fee9f334ae8c7c82f5eb17
manajay <manajay.dlj@gmail.com>
2024-10-21 17:28:23 +0800

    增加 tags 请求

--- service/ApiService.ts
+++ service/ApiService.ts
@@ -9,10 +9,22 @@ export interface IBlog {
   excerpt: string;
   url: string;
   html: string;
+  reading_time: string; //建议阅读时间
+}
+
+export interface ITag {
+  id: string;
+  name: string;
+  description: string;
+  feature_image: string;
+  slug: string;
+  url: string;
+  visibility: string; // public
 }
 
 export interface IApiService {
    getLatestBlog():Promise<IBlog[]>;
+   getTags():Promise<ITag[]>;
 }
 
 class ApiService implements IApiService {
@@ -20,19 +32,25 @@ class ApiService implements IApiService {
   }
   getLatestBlog(): Promise<IBlog[]> {
     return new Promise((resolve, reject) => {
-      const res = wx.cloud.callContainer({
-        config: {
-          env: WXCloudServiceEnvID,
-        },
-        path: '/api/blog/latest',
-        method: 'GET',
-        data: "",
-        header: {
-          'X-WX-SERVICE': WXServiceName,
-          "content-type": "application/json"
-        }
-      });
-      resolve(res);
+      // const res = wx.cloud.callContainer({
+      //   config: {
+      //     env: WXCloudServiceEnvID,
+      //   },
+      //   path: '/api/blog/latest',
+      //   method: 'GET',
+      //   data: "",
+      //   header: {
+      //     'X-WX-SERVICE': WXServiceName,
+      //     "content-type": "application/json"
+      //   }
+      // });
+      reject();
+    });
+  }
+
+  getTags(): Promise<ITag[]> {
+    return new Promise((resolve, reject) => {
+      reject();
     });
   }
 }
@@ -43,13 +61,27 @@ class ApiWXService implements IApiService {
   getLatestBlog(): Promise<IBlog[]> {
     return new Promise((resolve, reject) => {
       wx.request({
-        url: GhostBaseUrl + '/posts/?key=' + GhostContentKey + '&fields=id,title,url,published_at,feature_image,html,excerpt&limit=10&page=1',
+        url: GhostBaseUrl + '/posts/?key=' + GhostContentKey + '&fields=id,title,url,published_at,feature_image,html,excerpt,reading_time&limit=10&page=1',
         header: {
           'content-type': 'application/json' // 默认值
         },
         success(res) {
           const data = res?.data as { posts };
           resolve(data?.posts);
+        }
+      })
+    });
+  }
+  getTags(): Promise<ITag[]> {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: GhostBaseUrl + '/tags/?key=' + GhostContentKey + '&order=name asc',
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success(res) {
+          const data = res?.data as { tags };
+          resolve(data?.tags);
         }
       })
     });
